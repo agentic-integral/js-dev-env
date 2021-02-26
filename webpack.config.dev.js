415ff06a896613e09747e9a5bc992cdec5085443
Travis Brannon <development@inspiresportaustralia.com>
2021-02-26 16:00:17 +0100

    Initial Setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: [path.resolve(__dirname, "src/index")],
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+};
